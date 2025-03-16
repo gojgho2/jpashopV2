758a259f60eaf286689ec98c8867038172699c9b
ITO_MZ_GOJGHO\mz_gojgho <gojgho@mz.co.kr>
2025-03-16 14:08:41 +0900

    study signUp

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +17,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty(message = "이름은 필수값입니다.")
     private String name;
 
     @Embedded
